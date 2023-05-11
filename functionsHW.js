912dde3d0b4b2ba3b4a11574c4ede278eff5c429
Nataliia Kravchuk <kr.nata5@gmail.com>
2023-05-11 07:34:43 +0300

    comit

---
+++ functionsHW.js
@@ -0,0 +1,12 @@
+//1...........................................
+function food(hamburger,fries){
+    if (hamburger == 3 && fries == 1) {
+        console.log('Ми поїли');
+        } else if (hamburger <= 3 && fries == 1) {
+        console.log('Ми ідемо в інше кафе');
+        }
+        }
+
+    food(3,1)
+    food(2,1)
+    food(1,1)
